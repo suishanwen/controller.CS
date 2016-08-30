e3ff0e2848db1609a45ca70875dee8b5ff778d0b
sw <suishanwen@icloud.com>
2016-08-30 18:50:44 +0800

    autoVoteSystem restructure

--- controller/Form3.cs
+++ controller/Form3.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -20,7 +22,11 @@ namespace controller
         private int kick;
         private string arrDrop;
         private string activeVm;
-
+        private Thread autoVote;
+        private VoteProject activeVoteProject;
+        private List<VoteProject> voteProjectMonitorList = new List<VoteProject>();
+        private string voteProjectNameDroped = IniReadWriter.ReadIniKeys("Command", "voteProjectNameDroped", "./project.ini");
+        private string voteProjectNameGreen = IniReadWriter.ReadIniKeys("Command", "voteProjectNameGreen", "./project.ini");
 
         public Form3()
         {
@@ -31,6 +37,192 @@ namespace controller
             _mainForm = form1;
             InitializeComponent();
             timer1.Enabled = true;
+            autoVote = new Thread(autoVoteSystem);
+            autoVote.Start();
+        }
+
+
+        //关闭程序，结束自动挂票线程
+        private void form3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            autoVote.Abort();
+        }
+
+        private bool isDropedProject(string project, int checkType)
+        {
+            if (checkType == 1)
+            {
+                string[] dropedProjectList = voteProjectNameDroped.Split('|');
+                foreach (string dropedProject in dropedProjectList)
+                {
+                    if (project.IndexOf(dropedProject) != -1)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            return voteProjectNameDroped.IndexOf(project) != -1;
+        }
+
+        private bool isGreenProject(string project, int checkType)
+        {
+
+            string[] greenProjectList = voteProjectNameGreen.Split('|');
+            foreach (string greenProject in greenProjectList)
+            {
+                if (project.IndexOf(greenProject) != -1)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        private List<VoteProject> getVoteProjects()
+        {
+            HttpManager httpUtil = HttpManager.getInstance();
+            string result = httpUtil.requestHttpGet("http://butingzhuan.com/tasks.php", "", "");
+            result = result.Substring(result.IndexOf("时间</td>"));
+            result = result.Substring(0, result.IndexOf("qzd_yj"));
+            result = result.Substring(result.IndexOf("<tr class='blank'>"));
+            result = result.Substring(0, result.LastIndexOf("<tr class='blank'>"));
+            Log.writeLogs("./log.txt", "Finished Request!");
+            Regex regTR = new Regex(@"(?is)<tr[^>]*>(?:(?!</tr>).)*</tr>");
+            Regex regTD = new Regex(@"(?is)<t[dh][^>]*>((?:(?!</td>).)*)</t[dh]>");
+            MatchCollection mcTR = regTR.Matches(result);
+            List<VoteProject> voteProjectList = new List<VoteProject>();
+            foreach (Match mTR in mcTR)
+            {
+                if (mTR.Value.IndexOf("不换") == -1 && !isDropedProject(mTR.Value, 1))
+                {
+                    MatchCollection mcTD = regTD.Matches(mTR.Value);
+                    int index = 0;
+                    VoteProject voteProject = new VoteProject();
+                    foreach (Match mTD in mcTD)
+                    {
+                        string innerTd = mTD.Groups[1].Value;
+                        Log.writeLogs("./log.txt", "***"+index);
+                        Log.writeLogs("./log.txt", mTD.Value);
+                        switch (index)
+                        {
+                            case 2:
+                                voteProject.ProjectName = HtmlMatch.GetContent(innerTd, "a");
+                                break;
+                            case 5:
+                                voteProject.Price = double.Parse(innerTd);
+                                break;
+                            case 7:
+                                String[] quantityInfo = mTD.Value.Split('"');
+                                quantityInfo = quantityInfo[1].Split('/');
+                                try
+                                {
+                                    voteProject.Remains = long.Parse(innerTd.Trim());
+                                    if (!StringUtil.isEmpty(quantityInfo[0].Trim()))
+                                    {
+
+                                        voteProject.FinishQuantity = long.Parse(quantityInfo[0]);
+
+                                    }
+                                    voteProject.TotalRequire = long.Parse(quantityInfo[1].Substring(0, quantityInfo[1].IndexOf(" ")));
+
+                                }catch(Exception e) { }
+                                break;
+                            case 8:
+                                voteProject.BackgroundAddress = HtmlMatch.GetAttr(innerTd, "a", "href");
+                                break;
+                            case 9:
+                                voteProject.DownloadAddress = HtmlMatch.GetAttr(innerTd, "a", "href");
+                                break;
+                            case 10:
+                                try
+                                {
+                                    voteProject.IdType = HtmlMatch.GetAttr(innerTd, "input", "value").Substring(0, 2);
+                                }
+                                catch (Exception e)
+                                {
+                                    if (innerTd.IndexOf("BT-") != -1)
+                                    {
+                                        voteProject.IdType = "BT";
+                                    }
+                                    else if (innerTd.IndexOf("AQ-") != -1)
+                                    {
+                                        voteProject.IdType = "AQ";
+                                    }
+                                    else if (innerTd.IndexOf("Q7-") != -1)
+                                    {
+                                        voteProject.IdType = "Q7";
+                                    }
+                                }
+                                break;
+                            case 12:
+                                voteProject.BackgroundNo = innerTd;
+                                break;
+                            case 13:
+                                voteProject.RefreshDate = Convert.ToDateTime("2016-" + innerTd + ":00");
+                                break;
+                        }
+                        index++;
+                    }
+                    voteProject.IsRestrict = voteProject.BackgroundNo.IndexOf("限制") != -1;
+                    voteProjectList.Add(voteProject);
+                }
+            }
+            return voteProjectList;
+        }
+
+        private void voteProjectsAnalysis(List<VoteProject> voteProjectList)
+        {
+            foreach (VoteProject voteProject in voteProjectList)
+            {
+                //不存在于黑名单，并且是九天项目
+                if (!isDropedProject(voteProject.ProjectName, 0) && voteProject.BackgroundAddress.IndexOf("http://61.153.107.108") != -1)
+                {
+                    bool exist = false;
+                    for (int i = 0; i < voteProjectMonitorList.Count; i++)
+                    {
+                        if (voteProject.ProjectName.Equals(voteProjectMonitorList[i].ProjectName))
+                        {
+                            exist = true;
+                            break;
+                        }
+                    }
+                    if (!exist)
+                    {
+                        voteProjectMonitorList.Add(voteProject);
+                    }
+                }
+            }
+        }
+
+        private void testVoteProjectMonitorList()
+        {
+            int i = 0;
+            foreach (VoteProject voteProject in voteProjectMonitorList)
+            {
+                if (i == 0)
+                {
+                    HttpManager httpManager = HttpManager.getInstance();
+                    httpManager.HttpDownloadFile(voteProject.DownloadAddress, IniReadWriter.ReadIniKeys("Command", "Downloads", _mainForm.PathShare+"/CF.ini")+ voteProject.DownloadAddress.Substring(voteProject.DownloadAddress.LastIndexOf("/")));
+                }
+                Console.WriteLine("projectName：" + voteProject.ProjectName + ",price：" + voteProject.Price + ",remains：" + voteProject.Remains);
+                i++;
+            }
+        }
+
+        private void autoVoteSystem()
+        {
+            Log.writeLogs("./log.txt", "");
+            Log.writeLogs("./log.txt", "AutoVoteSystem Thread Running");
+            do
+            {
+                voteProjectsAnalysis(getVoteProjects());
+                testVoteProjectMonitorList();
+                Thread.Sleep(30000);
+            }
+            while (true);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
---
+++ controller/util/Log.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controller.util
+{
+    class Log
+    {
+        //写txt
+        public static void writeLogs(string pathName, string content)
+        {
+            if (content.Equals(""))
+            {
+                StreamWriter sw = new StreamWriter(pathName);
+                sw.Write("");
+                sw.Close();
+            }
+            else
+            {
+                StreamWriter sw = File.AppendText(pathName);
+                sw.WriteLine(content);
+                sw.Close();
+            }
+        }
+    }
+}
