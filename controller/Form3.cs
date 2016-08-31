db0b98935f911632da47e8e6bc5f2f0ce83e4bee
sw <suishanwen@icloud.com>
2016-08-31 16:11:15 +0800

    restructure

--- controller/Form3.cs
+++ controller/Form3.cs
@@ -6,9 +6,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace controller
@@ -25,8 +26,9 @@ namespace controller
         private Thread autoVote;
         private VoteProject activeVoteProject;
         private List<VoteProject> voteProjectMonitorList = new List<VoteProject>();
-        private string voteProjectNameDroped = IniReadWriter.ReadIniKeys("Command", "voteProjectNameDroped", "./project.ini");
-        private string voteProjectNameGreen = IniReadWriter.ReadIniKeys("Command", "voteProjectNameGreen", "./project.ini");
+        private string voteProjectNameDroped;
+        private string voteProjectNameGreen;
+        private int downLoadCount;
 
         public Form3()
         {
@@ -37,6 +39,7 @@ namespace controller
             _mainForm = form1;
             InitializeComponent();
             timer1.Enabled = true;
+            //timer2.Enabled = false;
             autoVote = new Thread(autoVoteSystem);
             autoVote.Start();
         }
@@ -50,6 +53,7 @@ namespace controller
 
         private bool isDropedProject(string project, int checkType)
         {
+            voteProjectNameDroped = IniReadWriter.ReadIniKeys("Command", "voteProjectNameDroped", _mainForm.PathShare + "/AutoVote.ini");
             if (checkType == 1)
             {
                 string[] dropedProjectList = voteProjectNameDroped.Split('|');
@@ -69,6 +73,7 @@ namespace controller
         private bool isGreenProject(string project, int checkType)
         {
 
+            voteProjectNameGreen = IniReadWriter.ReadIniKeys("Command", "voteProjectNameGreen", _mainForm.PathShare + "/AutoVote.ini");
             string[] greenProjectList = voteProjectNameGreen.Split('|');
             foreach (string greenProject in greenProjectList)
             {
@@ -104,8 +109,8 @@ namespace controller
                     foreach (Match mTD in mcTD)
                     {
                         string innerTd = mTD.Groups[1].Value;
-                        Log.writeLogs("./log.txt", "***"+index);
-                        Log.writeLogs("./log.txt", mTD.Value);
+                        //Log.writeLogs("./log.txt", "***"+index);
+                        //Log.writeLogs("./log.txt", mTD.Value);
                         switch (index)
                         {
                             case 2:
@@ -128,7 +133,8 @@ namespace controller
                                     }
                                     voteProject.TotalRequire = long.Parse(quantityInfo[1].Substring(0, quantityInfo[1].IndexOf(" ")));
 
-                                }catch(Exception e) { }
+                                }
+                                catch (Exception e) { }
                                 break;
                             case 8:
                                 voteProject.BackgroundAddress = HtmlMatch.GetAttr(innerTd, "a", "href");
@@ -197,19 +203,92 @@ namespace controller
             }
         }
 
+
+        private void writeAutoVoteProject()
+        {
+            if (activeVoteProject == null)
+            {
+                SwitchUtil.clearAutoVote(_mainForm.PathShare);
+            }
+            else
+            {
+                IniReadWriter.WriteIniKeys("Command", "ProjectName", activeVoteProject.ProjectName, _mainForm.PathShare + "/AutoVote.ini");
+                IniReadWriter.WriteIniKeys("Command", "Price", activeVoteProject.Price.ToString(), _mainForm.PathShare + "/AutoVote.ini");
+                IniReadWriter.WriteIniKeys("Command", "TotalRequire", activeVoteProject.TotalRequire.ToString(), _mainForm.PathShare + "/AutoVote.ini");
+                IniReadWriter.WriteIniKeys("Command", "FinishQuantity", activeVoteProject.FinishQuantity.ToString(), _mainForm.PathShare + "/AutoVote.ini");
+                IniReadWriter.WriteIniKeys("Command", "Remains", activeVoteProject.Remains.ToString(), _mainForm.PathShare + "/AutoVote.ini");
+                IniReadWriter.WriteIniKeys("Command", "BackgroundNo", activeVoteProject.BackgroundNo, _mainForm.PathShare + "/AutoVote.ini");
+                IniReadWriter.WriteIniKeys("Command", "BackgroundAddress", activeVoteProject.BackgroundAddress, _mainForm.PathShare + "/AutoVote.ini");
+                IniReadWriter.WriteIniKeys("Command", "DownloadAddress", activeVoteProject.DownloadAddress, _mainForm.PathShare + "/AutoVote.ini");
+                IniReadWriter.WriteIniKeys("Command", "IsRestrict", activeVoteProject.IsRestrict.ToString(), _mainForm.PathShare + "/AutoVote.ini");
+                IniReadWriter.WriteIniKeys("Command", "IdType", activeVoteProject.IdType, _mainForm.PathShare + "/AutoVote.ini");
+                IniReadWriter.WriteIniKeys("Command", "RefreshDate", activeVoteProject.RefreshDate.ToLocalTime().ToString(), _mainForm.PathShare + "/AutoVote.ini");
+            }
+        }
+
+        private void setWorkerId()
+        {
+            if (activeVoteProject.IdType.Equals("Q7"))
+            {
+                IniReadWriter.WriteIniKeys("Command", "worker", "Q7-21173", _mainForm.PathShare + "/CF.ini");
+            }
+            else
+            {
+                IniReadWriter.WriteIniKeys("Command", "worker", "AQ-239356", _mainForm.PathShare + "/CF.ini");
+            }
+        }
+
         private void testVoteProjectMonitorList()
         {
-            int i = 0;
-            foreach (VoteProject voteProject in voteProjectMonitorList)
+            for (int i = 0; i < voteProjectMonitorList.Count; i++)
             {
-                if (i == 0)
+                VoteProject voteProject = voteProjectMonitorList[i];
+                if (voteProject.Remains > 0 && (voteProject.Remains * voteProject.Price) > 100 && !voteProject.IsRestrict)
                 {
+                    Console.WriteLine("projectName：" + voteProject.ProjectName + ",price：" + voteProject.Price + ",remains：" + voteProject.Remains);
                     HttpManager httpManager = HttpManager.getInstance();
-                    httpManager.HttpDownloadFile(voteProject.DownloadAddress, IniReadWriter.ReadIniKeys("Command", "Downloads", _mainForm.PathShare+"/CF.ini")+ voteProject.DownloadAddress.Substring(voteProject.DownloadAddress.LastIndexOf("/")));
+                    string pathName = IniReadWriter.ReadIniKeys("Command", "Downloads", _mainForm.PathShare + "/CF.ini") + "\\" + voteProject.DownloadAddress.Substring(voteProject.DownloadAddress.LastIndexOf("/") + 1);
+                    string url = voteProject.DownloadAddress;
+                    string now = DateTime.Now.ToLocalTime().ToString();
+                    Log.writeLogs("./log.txt", "开始下载:" + url);
+                    downLoadCount = 0;
+                    //timer2.Enabled = true;
+                    httpManager.HttpDownloadFile(url, pathName);
+                    //timer2.Enabled = false;
+                    Log.writeLogs("./log.txt", pathName + "  下载完成");
+                    Winrar.UnCompressRar(_mainForm.PathShare + "/投票项目/" + voteProject.ProjectName, IniReadWriter.ReadIniKeys("Command", "Downloads", _mainForm.PathShare + "/CF.ini"), voteProject.DownloadAddress.Substring(voteProject.DownloadAddress.LastIndexOf("/") + 1));
+                    try
+                    {
+                        File.Delete(pathName);
+
+                    }
+                    catch (IOException)
+                    {
+                        Log.writeLogs("./log.txt", pathName + "-->文件占用中，无法删除!");
+                    }
+                    activeVoteProject = voteProject;
+                    writeAutoVoteProject();
+                    setWorkerId();
+                    _mainForm.VM3TextBox.Text = "";
+                    Log.writeLogs("./log.txt", "AutoVote: " + voteProject.ProjectName + " " + voteProject.BackgroundNo);
+                    SwitchUtil.swichVm(_mainForm.VM1, _mainForm.VM2, _mainForm.VM3TextBox, _mainForm.PathShareVm + "\\投票项目\\" + voteProject.ProjectName + "\\vote.exe", "投票项目", _mainForm.PathShare);
+                    break;
                 }
-                Console.WriteLine("projectName：" + voteProject.ProjectName + ",price：" + voteProject.Price + ",remains：" + voteProject.Remains);
-                i++;
             }
+
+        }
+
+        private bool isWaitOrder()
+        {
+            for (int i = int.Parse(_mainForm.VM1); i <= int.Parse(_mainForm.VM2); i++)
+            {
+                string taskName = IniReadWriter.ReadIniKeys("Command", "TaskName" + i, _mainForm.PathShare + "/Task.ini");
+                if (!taskName.Equals("待命"))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         private void autoVoteSystem()
@@ -219,7 +298,10 @@ namespace controller
             do
             {
                 voteProjectsAnalysis(getVoteProjects());
-                testVoteProjectMonitorList();
+                if (isWaitOrder())
+                {
+                    testVoteProjectMonitorList();
+                }
                 Thread.Sleep(30000);
             }
             while (true);
@@ -344,5 +426,16 @@ namespace controller
 
             }
         }
+
+        //private void timer2_Tick(object sender, EventArgs e)
+        //{
+        //    downLoadCount++;
+        //    if (downLoadCount > 150)
+        //    {
+        //        autoVote.Abort();
+        //        autoVote.Start();
+        //        timer2.Enabled = false;
+        //    }
+        //}
     }
 }
