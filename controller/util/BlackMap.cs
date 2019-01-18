0fd44b86894a79f95c44fc08419e236bcde448ca
sw <suishanwen@icloud.com>
2019-01-18 10:30:44 +0800

    U

---
+++ controller/util/BlackMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace controller.util
+{
+    class BlackMap
+    {
+        private string name;
+        private double price;
+
+        public string Name { get => name; set => name = value; }
+        public double Price { get => price; set => price = value; }
+    }
+}
